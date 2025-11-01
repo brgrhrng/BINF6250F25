a8c4f83118c6c3e2021f3c3a91c8a2a1086d3c81
Brooks Groharing <brooks.groharing@gmail.com>
2025-11-01 02:56:58 -0400

    init markov_models library

---
+++ projects/project08/markov_models.py
@@ -0,0 +1,22 @@
+##########################################################################################
+#   markov_models
+#   A library of objects and functions for constructing and processing 
+#   Hidden Markov Models (HMM)
+#   Created by Jacque Caldwell and Brooks Groharing for BINF6250 at Northeastern University
+###########################################################################################
+
+
+class MyHMM: #TODO: come up with a not terrible name for this class
+  """
+  class description
+  """
+   def __init__(self):
+     pass
+
+
+class HiddenState:
+  """
+  class description
+  """
+  def __init__(self):
+     pass
