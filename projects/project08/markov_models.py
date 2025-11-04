a853534b323e0e22f52cdf4ae81a2fa27688ac5b
Brooks Groharing <brooks.groharing@gmail.com>
2025-11-04 04:47:10 -0500

    condense extraneous whitespace

--- projects/project08/markov_models.py
+++ projects/project08/markov_models.py
@@ -14,10 +14,8 @@ class HiddenState:
   Internal values:
     name: a unique name representing the state
     init_prob: probability of state coming from the start node
-    
     out_states: list of HiddenState names representing outbound transitions
     out_probs: list of probabilities of transitioning to each out_state
-    
     emissions, emission_probs: list of possible emissions and their probabilities
     """
   def __init__(self, name: str, init_prob: float, emissions_dict: dict[str,float]):
@@ -32,16 +30,12 @@ class HiddenState:
     """
     self.name = name
     self.init_prob = init_prob
-    
     self.emission_probs = emissions_dict
-    
-    self.transition_to = {} # initial empty as a deadend
+    self.transition_to = {} # initialize state as a "dead end"
     
     
   def set_transitions(self, transitions_dict):
-    """
-    Update outgoing edges to match provided transitions_dict.
-    """
+    """ Update outgoing edges to match provided transitions_dict. """
     self.transition_to = transitions_dict
   
   
@@ -73,78 +67,42 @@ class HMM:
     self.states = [] # container for HiddenState objects
     
     state_names = emit_probs.keys()
-    
     for state_name in state_names:
       new_state = HiddenState(state_name, init_prob = init_probs[state_name], emissions_dict = emit_probs[state_name])
-      
       if state_name in trans_probs.keys():
         new_state.set_transitions(trans_probs[state_name])
         
       self.states.append(new_state)
       
-  
-  def __traceback_viterbi__(self, traceback_pos, backptrs):
-    ''' private function that traces back the backpointers, 
-        obtaining the most probable sequence of states up to traceback_pos
-    
-    args: 
-      traceback_pos: pos to start traceback of backptrs.
-      backpointers: backpointers to create our final traceback of hidden states
       
-    returns:  list of strings that show the hidden states
-    '''
-    tb_obs_i, state_i = traceback_pos
-    tb_obs_i, state_i = int(tb_obs_i), int(state_i)
-    
-    state_names = []
-    
-    for obs_i in range(tb_obs_i, 0, -1): # loop backwards through observations
-      state_names.append(self.states[state_i].name) # Save current state name
-      state_i = int(backptrs[state_i, obs_i]) # update index to pointer
-    
-    state_names = reversed(state_names)
-  
-    return state_names
-    
-    
   def run_viterbi(self, observations):
     """
     Run the viterbi algorithm to determine the most likely sequence of states
     that would produce a given set of observations under this model.
-    
-    args:
+    Args:
       observations: a list of observation values, or a string where each 
                     character is 1 observation.
-    
     Returns: list of state names
     """
-    
-    if type(observations) == str:
+    if type(observations) == str: # convert str -> list(char)
       observations = [char for char in observations]
     
-    if not type(observations) is list:
+    if not type(observations) is list: # Verify type.
       raise Exception("\'observations\' must be a list or string.")
     
     v_matrix, backptrs = self.__fill_viterbi_matrix__(observations)
     
-    ypos = np.argmax(v_matrix[:,-1])
-    xpos = v_matrix.shape[1]-1
-    
-    print(v_matrix.shape, xpos, ypos)
-    
-    traceback = self.__traceback_viterbi__((xpos,ypos), backptrs)
-    
-    # traceback will be ycoord to index the list of strings "IIIGGGGGGIII"
+    obs_i = v_matrix.shape[1]-1
+    state_i = np.argmax(v_matrix[:,-1])
+    traceback = self.__traceback_viterbi__((obs_i,state_i), backptrs)
+    return traceback
 
-    print (v_matrix,backptrs)
-    print (traceback)
-
+    
   def __fill_viterbi_matrix__(self, observations, log_values = True):
     '''Creates a Viterbi matrix and backpointers
         1. Initialize the viterbi and traceback matrices
         2. populate the matrices one by one.
-    
-    args: 
+    Args: 
       observations: a 1d list where each item represents one observation
       log_values (optional):  set to False to return flat p-values.
                               This will result in underflow errors!
@@ -187,7 +145,6 @@ class HMM:
     # and the index of the prior cell in this path (within its column) to
     # backpointers.
     for obs_i, observation in enumerate(observations[1:], start=1): # skip col 0
-      
       prior_path_probs = v_matrix[:,obs_i-1] # vector representing last column in v_matrix
       
       for state_i, current_state in enumerate(self.states):
@@ -198,7 +155,7 @@ class HMM:
         if log_values:
           total_path_probs = prior_path_probs
           total_path_probs += np.log10(trans_here_probs)
-          total_path_probs += np.log10(p_current_emission) # adds a scalar value
+          total_path_probs += np.log10(p_current_emission) # scalar value
         else:
           total_path_probs = prior_path_probs * trans_here_probs * p_current_emission
           
@@ -212,6 +169,28 @@ class HMM:
     print(f"v_matrix {v_matrix} backpointers {backpointers}")
     
     return(v_matrix, backpointers)
+  
+  
+  def __traceback_viterbi__(self, traceback_pos, backptrs):
+    ''' private function that traces back the backpointers, 
+        obtaining the most probable sequence of states up to traceback_pos
+    
+    args: 
+      traceback_pos: pos to start traceback of backptrs.
+      backpointers: backpointers to create our final traceback of hidden states
+      
+    returns:  list of strings that show the hidden states
+    '''
+    tb_obs_i, state_i = traceback_pos
+    tb_obs_i, state_i = int(tb_obs_i), int(state_i)
+    
+    state_names = []
+    for obs_i in range(tb_obs_i, 0, -1): # loop backwards through observations
+      state_names.append(self.states[state_i].name) # Save current state name
+      state_i = int(backptrs[state_i, obs_i]) # update index to pointer
+    
+    state_names = reversed(state_names)
+    return state_names
   
 
 # Example data provided in project description
